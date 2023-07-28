2691ea937e5055abeedff39dcac1db669d6ad155
Milan Forsthövel <mforstho@student.codam.nl>
2023-07-28 13:50:14 +0200

    Added extra newlines in output for readability

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mforstho <mforstho@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/24 16:19:51 by mforstho      #+#    #+#                 */
-/*   Updated: 2023/05/24 16:30:36 by mforstho      ########   odam.nl         */
+/*   Updated: 2023/07/28 13:49:10 by mforstho      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ int	main(void) {
 	testdata->nbr = 42;
 
 	std::cout << testdata << std::endl;
-	std::cout << testdata->nbr << std::endl;
+	std::cout << testdata->nbr << std::endl << std::endl;
 
 	p = Serializer::serialize(testdata);
-	std::cout << p << std::endl;
+	std::cout << p << std::endl << std::endl;
 
 	checkdata = Serializer::deserialize(p);
 	std::cout << checkdata << std::endl;
-	std::cout << checkdata->nbr << std::endl;
+	std::cout << checkdata->nbr << std::endl << std::endl;
 
 	p = Serializer::serialize(checkdata);
 	std::cout << p << std::endl;
